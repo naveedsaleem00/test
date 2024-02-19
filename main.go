f9395615d71fdf97dd2993e34697ef16693e4a4b
Muhammad Naveed Saleem <naveedsaleem@vizzweb.com>
2024-02-19 16:15:18 +0500

    test new commit

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/robfig/cron/v3"
+	"time"
 )
 
 func main() {
 
 	abc()
 
-	// Create a new cron scheduler
+	/*Create a new cron scheduler*/
 	go CronJob()
 
 }
 
 func CronJob() {
-	// Create a new cron scheduler
+	/*Create a new cron scheduler*/
 	c := cron.New()
 
-	// Add a cron job (in this case, it prints a message every minute)
+	/*Add a cron job (in this case, it prints a message every minute)*/
 	_, err := c.AddFunc("@every 5s", func() {
+		fmt.Println(time.Now())
 		abc()
 	})
 	if err != nil {
@@ -27,7 +29,7 @@ func CronJob() {
 		return
 	}
 
-	// Start the cron scheduler
+	/*Start the cron scheduler*/
 	c.Start()
 
 	fmt.Println("Cron job removed. Exiting...")
